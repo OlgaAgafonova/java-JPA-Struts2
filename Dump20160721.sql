8d06a00b5a2f5c266209f5bd1dc79ec1f2c6ecf1
OlgaAgafonova <volga.ahafonava@gmail.com>
2016-07-21 18:10:22 +0300

    Added dump of my DataBase

---
+++ Dump20160721.sql
@@ -0,0 +1,214 @@
+CREATE DATABASE  IF NOT EXISTS `mydbtest` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `mydbtest`;
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mydbtest
+-- ------------------------------------------------------
+-- Server version	5.7.13-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `address` (
+  `id_address` int(11) NOT NULL AUTO_INCREMENT,
+  `country` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `street` varchar(45) DEFAULT NULL,
+  `house` varchar(45) DEFAULT NULL,
+  `zip_code` varchar(6) DEFAULT NULL,
+  PRIMARY KEY (`id_address`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `address`
+--
+
+LOCK TABLES `address` WRITE;
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+INSERT INTO `address` VALUES (1,'USA','Ann Arbor','Green Avenue','5, Apt. 3','481047'),(2,'UK','London','Lombard Str.','20','459786'),(3,'Belarus','Novopolotsk','ul. Slobodskaya','8','211443'),(8,'Belarus','Minsk','ul. Yasnaya','10','220138');
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `job_place`
+--
+
+DROP TABLE IF EXISTS `job_place`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `job_place` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id_organization` int(11) DEFAULT NULL,
+  `id_position` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `job_place`
+--
+
+LOCK TABLES `job_place` WRITE;
+/*!40000 ALTER TABLE `job_place` DISABLE KEYS */;
+INSERT INTO `job_place` VALUES (1,'2001-10-23','2005-01-15',18,1,5),(2,'2001-10-23','2005-01-15',19,2,4),(3,'2001-10-23','2005-01-15',32,3,5),(4,'2001-10-23','2005-01-15',36,3,3),(5,'2001-10-23','2005-01-15',38,1,5),(6,'2001-10-23','2005-01-15',40,2,2),(7,'2001-10-23','2005-01-15',46,2,5),(8,'2001-10-23','2005-01-15',47,1,1),(9,'2001-10-23','2005-01-15',48,3,5),(10,'2001-10-23','2005-01-15',49,3,1),(11,'2001-10-23','2005-01-15',50,1,1);
+/*!40000 ALTER TABLE `job_place` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `organization`
+--
+
+DROP TABLE IF EXISTS `organization`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `organization` (
+  `id_organization` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `id_address` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_organization`),
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  KEY `id_address_idx` (`id_address`),
+  CONSTRAINT `id_address` FOREIGN KEY (`id_address`) REFERENCES `address` (`id_address`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `organization`
+--
+
+LOCK TABLES `organization` WRITE;
+/*!40000 ALTER TABLE `organization` DISABLE KEYS */;
+INSERT INTO `organization` VALUES (1,'Chamomile',2),(2,'Carnation',1),(3,'Violet',3),(5,'Octopus',8);
+/*!40000 ALTER TABLE `organization` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `position`
+--
+
+DROP TABLE IF EXISTS `position`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `position` (
+  `id_position` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_position`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `position`
+--
+
+LOCK TABLES `position` WRITE;
+/*!40000 ALTER TABLE `position` DISABLE KEYS */;
+INSERT INTO `position` VALUES (3,'Business-process automation Specialist'),(1,'IT Director'),(2,'LAN Administrator'),(5,'QA'),(4,'Software developer');
+/*!40000 ALTER TABLE `position` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'USER'),(2,'ADMIN');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(45) DEFAULT NULL,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (18,'lodsfn','Mike','Vlasov','x@ma.s'),(19,'lssfn','Kevin','kline','f@ma.s'),(32,'orry','sasha','serov','sasha@r.o'),(36,'login','Calvin','kline','s@ma.s'),(38,'lssfn','Kevin','kline','f@ma.s'),(40,'lodsfn','Mike','Vlasovez','x@ma.s'),(46,'orry','alexo','serov','sasha@r.o'),(47,'orry','Alexey','serov','sasha@r.o'),(48,'orry','Alexander','serov','sasha@r.o'),(49,'orry','ale','serov','sasha@r.o'),(50,'orry','Aleco','serov','sasha@r.o');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` varchar(45) DEFAULT NULL,
+  `id_role` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` VALUES (1,'18','1'),(2,'18','1'),(3,'18','1'),(4,'18','2'),(5,'19','1'),(9,'38','1'),(14,'32','2'),(15,'32','1'),(20,'40','1'),(21,'40','1'),(36,'47','1'),(37,'47','2'),(38,'47','1'),(39,'47','2'),(40,'48','1'),(41,'48','2'),(42,'48','1'),(43,'48','2'),(48,'50','2'),(49,'50','1'),(50,'50','2'),(51,'50','1'),(57,'36','2'),(60,'49','2'),(61,'49','1'),(64,'46','2'),(65,'46','1');
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-07-21 18:07:15
